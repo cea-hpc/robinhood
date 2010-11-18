ccdbc80189c46cf4feaa7a93de8098a2399aa112
Thomas LEIBOVICI - CEA/DAM <thomas.leibovici@cea.fr>
2010-11-18 14:31:44 +0100

    Activating HSM coordinator for Lustre-HSM tests

--- tests/lustre_fs/1-test_setup.sh
+++ tests/lustre_fs/1-test_setup.sh
@@ -9,6 +9,14 @@ $CFG_SCRIPT empty_db robinhood_lustre
 $CFG_SCRIPT enable_chglogs lustre
 
 if [[ -z "$PURPOSE" || $PURPOSE = "LUSTRE_HSM" ]]; then
+	
+	echo -n "checking coordinator status: "
+	status=`cat /proc/fs/lustre/mdt/lustre-MDT0000/hsm_control`
+	echo $status
+
+	if [[ $status != "enabled" ]]; then
+		echo "enable" >  /proc/fs/lustre/mdt/lustre-MDT0000/hsm_control
+	fi
 
 	echo "Checking if copytool is already running..."
 	if (( `pgrep -f lhsmd_posix | wc -l` > 0 )); then
