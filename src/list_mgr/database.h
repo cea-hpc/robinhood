15601a13e41f2c343f14ee58683b01efb26e1dc7
Antoine Obert - CEA/DAM <antoine.obert.ocre@cea.fr>
2011-03-04 13:01:51 +0100

    Adding trigger creation/removal functions

--- src/list_mgr/database.h
+++ src/list_mgr/database.h
@@ -64,6 +64,12 @@ int            db_result_nb_records( db_conn_t * conn, result_handle_t * p_resul
 /* free result resources */
 int            db_result_free( db_conn_t * conn, result_handle_t * p_result );
 
+/* remove a trigger */
+int            db_drop_trigger( db_conn_t * conn, const char *name );
+
+/* create a trigger */
+int            db_create_trigger( db_conn_t * conn, const char *name, const char *event,
+                               const char *table, const char *body );
 
 /* -------------------- prepared statements management ---------------- */
 
