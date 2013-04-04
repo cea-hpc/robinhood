ee5853840005091d01a3ea6838453730612223f5
Thomas LEIBOVICI - CEA/DAM <thomas.leibovici@cea.fr>
2013-04-04 13:43:01 +0200

    don't clean old entries for partial scan if this results in removing an entry in backend storage

--- src/fs_scan/fs_scan.c
+++ src/fs_scan/fs_scan.c
@@ -341,8 +341,14 @@ static int TerminateScan( int scan_complete, time_t date_fin )
 
         ATTR_MASK_INIT( &op.fs_attrs );
 
-        /* if this is an initial scan, non-partial, don't rm old entries (but flush pipeline still) */
+        /* if this is an initial scan, don't rm old entries (but flush pipeline still) */
+        /* NB: don't clean old entries for partial scan: dangerous if
+         * files have been moved from one partition to another */
+#ifdef HAVE_RM_POLICY
+        if (is_first_scan || partial_scan_root)
+#else
         if (is_first_scan && !partial_scan_root)
+#endif
             op.callback_param = ( void * ) "End of flush";
         else
         {
@@ -351,12 +357,16 @@ static int TerminateScan( int scan_complete, time_t date_fin )
             ATTR( &op.fs_attrs, md_update ) = scan_start_time;
         }
 
+    /* don't clean old entries for partial scan: dangerous if
+     * files have been moved from one partition to another */
+#ifndef HAVE_RM_POLICY
         /* set root (if partial scan) */
         if (partial_scan_root)
         {
             ATTR_MASK_SET( &op.fs_attrs, fullpath );
             strcpy(ATTR(&op.fs_attrs, fullpath), partial_scan_root);
         }
+#endif
 
         /* set wait db flag */
         set_db_wait_flag(  );
