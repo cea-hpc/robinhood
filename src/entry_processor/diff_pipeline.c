cea-hpc/robinhood

commit e214f1f1e0325edec7cc53d72147f54cde4551d1

diff and scan: distinguish diff mask from display mask. Added new flavors for test 106 (diff).

src/entry_processor/diff_pipeline.c
@@ -745,21 +745,29 @@ int EntryProc_report_diff( struct entry_proc_op_t *p_op, lmgr_t * lmgr )
     /* Only keep fields that changed */
     if (p_op->db_op_type == OP_TYPE_UPDATE)
     {
-        /* XXX keep md_update to avoid removing the entry from DB:
-         * (the same for parent_id, name, path_update)
-         * use DB tag instead?
-         * TODO: distinguish display mask from attr mask.
+        int diff_mask = ListMgr_WhatDiff(&p_op->fs_attrs, &p_op->db_attrs);
+
+        /* In scan mode, always keep md_update and path_update,
+         * to avoid their cleaning at the end of the scan.
+         * Also keep name and parent as they are keys in DNAMES table.
          */
-        int to_keep = (ATTR_MASK_md_update | ATTR_MASK_parent_id
-                       | ATTR_MASK_name | ATTR_MASK_path_update)
-                      & p_op->fs_attrs.attr_mask;
+        int to_keep = ATTR_MASK_parent_id | ATTR_MASK_name;
 
-        ListMgr_KeepDiff(&p_op->fs_attrs, &p_op->db_attrs);
+        /* the mask to be displayed > diff_mask (include to_keep flags) */
+        int display_mask = entry_proc_conf.diff_mask & diff_mask;
 
-        /* keep only fields in diff_mask */
-        p_op->fs_attrs.attr_mask &= entry_proc_conf.diff_mask;
-        /* force initial mask we want to keep */
-        p_op->fs_attrs.attr_mask |= to_keep;
+        /* keep fullpath if parent or name changed (friendly display) */
+        if (diff_mask & (ATTR_MASK_parent_id | ATTR_MASK_name)) {
+            to_keep |= ATTR_MASK_fullpath;
+            display_mask |= ATTR_MASK_fullpath;
+        }
+#ifdef HAVE_CHANGELOGS
+        if (!p_op->extra_info.is_changelog_record)
+#endif
+            to_keep |= (ATTR_MASK_md_update | ATTR_MASK_path_update);
+
+        /* remove other unchanged attrs */
+        p_op->fs_attrs.attr_mask &= (diff_mask | to_keep);
 
         /* nothing changed => noop */
         if (p_op->fs_attrs.attr_mask == 0)
@@ -767,7 +775,7 @@ int EntryProc_report_diff( struct entry_proc_op_t *p_op, lmgr_t * lmgr )
             /* no op */
             p_op->db_op_type = OP_TYPE_NONE;
         }
-        else if (p_op->fs_attrs.attr_mask & entry_proc_conf.diff_mask)
+        else if (diff_mask & entry_proc_conf.diff_mask)
         {
             char attrchg[RBH_PATH_MAX] = "";
 
@@ -775,40 +783,30 @@ int EntryProc_report_diff( struct entry_proc_op_t *p_op, lmgr_t * lmgr )
             if (diff_arg->apply == APPLY_FS)
             {
                 /* attr from FS */
-                PrintAttrs(attrchg, RBH_PATH_MAX, &p_op->fs_attrs,
-                    p_op->fs_attrs.attr_mask & p_op->db_attrs.attr_mask
-                    & entry_proc_conf.diff_mask, 1);
-
+                PrintAttrs(attrchg, RBH_PATH_MAX, &p_op->fs_attrs, display_mask, 1);
                 printf("-"DFID" %s\n", PFID(&p_op->entry_id), attrchg);
 
                 /* attr from DB */
-                if ((p_op->db_attrs.attr_mask & p_op->fs_attrs.attr_mask
-                    & entry_proc_conf.diff_mask) == 0)
+                if (display_mask == 0)
                     attrchg[0] = '\0';
                 else
                     PrintAttrs(attrchg, RBH_PATH_MAX, &p_op->db_attrs,
-                        p_op->db_attrs.attr_mask & p_op->fs_attrs.attr_mask
-                        & entry_proc_conf.diff_mask, 1);
+                               display_mask, 1);
 
                 printf("+"DFID" %s\n", PFID(&p_op->entry_id), attrchg);
             }
             else
             {
                 /* attr from DB */
-                if ((p_op->db_attrs.attr_mask & p_op->fs_attrs.attr_mask
-                    & entry_proc_conf.diff_mask) == 0)
+                if (display_mask == 0)
                     attrchg[0] = '\0';
                 else
-                    PrintAttrs(attrchg, RBH_PATH_MAX, &p_op->db_attrs,
-                        p_op->db_attrs.attr_mask & p_op->fs_attrs.attr_mask
-                        & entry_proc_conf.diff_mask, 1);
+                    PrintAttrs(attrchg, RBH_PATH_MAX, &p_op->db_attrs, display_mask, 1);
 
                 printf("-"DFID" %s\n", PFID(&p_op->entry_id), attrchg);
 
                 /* attr from FS */
-                PrintAttrs(attrchg, RBH_PATH_MAX, &p_op->fs_attrs,
-                    p_op->fs_attrs.attr_mask & entry_proc_conf.diff_mask, 1);
-
+                PrintAttrs(attrchg, RBH_PATH_MAX, &p_op->fs_attrs, display_mask, 1);
                 printf("+"DFID" %s\n", PFID(&p_op->entry_id), attrchg);
             }
         }

src/entry_processor/std_pipeline.c
@@ -1704,20 +1704,21 @@ int EntryProc_get_info_fs( struct entry_proc_op_t *p_op, lmgr_t * lmgr )
         DisplayLog( LVL_CRIT, ENTRYPROC_TAG, "Error %d acknowledging stage.", rc );
     return rc;
 
-skip_record:
 #ifdef HAVE_CHANGELOGS
+skip_record:
     if ( p_op->extra_info.is_changelog_record )
     /* do nothing on DB but ack the record */
         rc = EntryProcessor_Acknowledge( p_op, STAGE_CHGLOG_CLR, FALSE );
     else
-#endif
     /* remove the operation from pipeline */
         rc = EntryProcessor_Acknowledge( p_op, -1, TRUE );
 
     if ( rc )
         DisplayLog( LVL_CRIT, ENTRYPROC_TAG, "Error %d acknowledging stage.", rc );
     return rc;
+#endif
 
+#ifdef HAVE_CHANGELOGS
 rm_record:
     /* soft remove the entry, except if it was 'new' (not in backend)
      * or not in DB.
@@ -1742,6 +1743,7 @@ rm_record:
     if ( rc )
         DisplayLog( LVL_CRIT, ENTRYPROC_TAG, "Error %d acknowledging stage.", rc );
     return rc;
+#endif
 }
 
 
@@ -1838,7 +1840,6 @@ int EntryProc_db_apply( struct entry_proc_op_t *p_op, lmgr_t * lmgr )
     if (p_op->db_op_type != OP_TYPE_INSERT)
         ATTR_MASK_UNSET( &p_op->fs_attrs, creation_time );
 #endif
-    p_op->fs_attrs.attr_mask &= ~readonly_attr_set;
 
 #ifdef HAVE_CHANGELOGS
     /* handle nlink. We don't want the values from the filesystem if
@@ -1866,19 +1867,34 @@ int EntryProc_db_apply( struct entry_proc_op_t *p_op, lmgr_t * lmgr )
     /* Only update fields that changed */
     if (p_op->db_op_type == OP_TYPE_UPDATE)
     {
-        ListMgr_KeepDiff(&p_op->fs_attrs, &p_op->db_attrs);
+        int diff_mask = ListMgr_WhatDiff(&p_op->fs_attrs, &p_op->db_attrs);
+
+        /* In scan mode, always keep md_update and path_update,
+         * to avoid their cleaning at the end of the scan.
+         * Also keep name and parent as they are keys in DNAMES table.
+         */
+        int to_keep = ATTR_MASK_parent_id | ATTR_MASK_name;
+
+        /* the mask to be displayed > diff_mask (include to_keep flags) */
+        int display_mask = entry_proc_conf.diff_mask & diff_mask;
+
+        /* keep fullpath if parent or name changed (friendly display) */
+        if (diff_mask & (ATTR_MASK_parent_id | ATTR_MASK_name)) {
+            to_keep |= ATTR_MASK_fullpath;
+            display_mask |= ATTR_MASK_fullpath;
+        }
+#ifdef HAVE_CHANGELOGS
+        if (!p_op->extra_info.is_changelog_record)
+#endif
+            to_keep |= (ATTR_MASK_md_update | ATTR_MASK_path_update);
+
+        /* remove other unchanged attrs */
+        p_op->fs_attrs.attr_mask &= (diff_mask | to_keep);
 
         /* SQL req optimizations:
-         * if update policy == always and path is not changed, don't set path_update
-         * idem for fileclasses and md.
+         * if update policy == always and fileclass is not changed,
+         * don't set update timestamp.
          */
-#ifdef _HAVE_FID
-        if ((policies.updt_policy.path.policy == UPDT_ALWAYS)
-            && !ATTR_MASK_TEST( &p_op->fs_attrs, parent_id)
-            && !ATTR_MASK_TEST( &p_op->fs_attrs, name))
-            ATTR_MASK_UNSET(&p_op->fs_attrs, path_update);
-#endif
-
 #ifdef HAVE_PURGE_POLICY
         if ((policies.updt_policy.fileclass.policy == UPDT_ALWAYS)
             && !ATTR_MASK_TEST( &p_op->fs_attrs, release_class))
@@ -1897,25 +1913,21 @@ int EntryProc_db_apply( struct entry_proc_op_t *p_op, lmgr_t * lmgr )
             /* no op */
             p_op->db_op_type = OP_TYPE_NONE;
         }
-        else if (p_op->fs_attrs.attr_mask & entry_proc_conf.diff_mask)
+        /* something changed in diffmask */
+        else if (diff_mask & entry_proc_conf.diff_mask)
         {
             char attrchg[RBH_PATH_MAX] = "";
 
             /* attr from DB */
-            if ((p_op->db_attrs.attr_mask & p_op->fs_attrs.attr_mask
-                & entry_proc_conf.diff_mask) == 0)
+            if (display_mask == 0)
                 attrchg[0] = '\0';
             else
-                PrintAttrs(attrchg, RBH_PATH_MAX, &p_op->db_attrs,
-                    p_op->db_attrs.attr_mask & p_op->fs_attrs.attr_mask
-                    & entry_proc_conf.diff_mask, 1);
+                PrintAttrs(attrchg, RBH_PATH_MAX, &p_op->db_attrs, display_mask, 1);
 
             printf("-"DFID" %s\n", PFID(&p_op->entry_id), attrchg);
 
             /* attr from FS */
-            PrintAttrs(attrchg, RBH_PATH_MAX, &p_op->fs_attrs,
-                p_op->fs_attrs.attr_mask & entry_proc_conf.diff_mask, 1);
-
+            PrintAttrs(attrchg, RBH_PATH_MAX, &p_op->fs_attrs, display_mask, 1);
             printf("+"DFID" %s\n", PFID(&p_op->entry_id), attrchg);
         }
     }
@@ -1936,6 +1948,8 @@ int EntryProc_db_apply( struct entry_proc_op_t *p_op, lmgr_t * lmgr )
                 printf("--"DFID"\n", PFID(&p_op->entry_id));
         }
     }
+
+    p_op->fs_attrs.attr_mask &= ~readonly_attr_set;
 
     /* insert to DB */
     switch ( p_op->db_op_type )

tests/lustre_fs/2-run-tests.sh
@@ -3187,10 +3187,9 @@ function test_diff
 
 	clean_logs
 
-    # TODO flavors:
-    # scan + diff option (various)
-    # diff (various), no apply
-    # diff (various) + apply to DB
+    # diff: diff (various), no apply
+    # diffapply: diff (various) + apply to DB
+    # scan: scan with diff option (various)
 
     # populate filesystem
     mkdir $ROOT/dir.1 || error "mkdir"
@@ -3229,15 +3228,24 @@ function test_diff
 	lfs swap_layouts $ROOT/dir.2/e  $ROOT/dir.2/f || error "lfs swap_layouts"
     fi
 
-    echo "2-diff..."
-    $DIFF -f ./cfg/$config_file -l FULL > report.out 2> rh_report.log || error "performing diff"
+    # need 1s difference for md and name GC
+    sleep 1
+
+    echo "2-diff ($policy_str)..."
+    if [ "$flavor" = "diff" ]; then
+        $DIFF -f ./cfg/$config_file -l FULL > report.out 2> rh_report.log || error "performing diff"
+    elif [ "$flavor" = "diffapply" ]; then
+        $DIFF --apply=db -f ./cfg/$config_file -l FULL > report.out 2> rh_report.log || error "performing diff"
+    elif [ "$flavor" = "scan" ]; then
+        $RH -f ./cfg/$config_file -l FULL --scan --once --diff=all -L rh_report.log > report.out || error "performing scan+diff"
+    fi
 
     [ "$DEBUG" = "1" ] && cat report.out
 
     # must get:
     # new entries dir.1/file.new and dir.new
-    egrep -e '^++' report.out | grep -v '+++' | grep file.new | grep type=file || error "missing create dir.1/file.new"
-    egrep -e '^++' report.out | grep -v '+++' | grep dir.new | grep type=dir || error "missing create dir.new"
+    egrep '^++' report.out | grep -v '+++' | grep -E "name='file.new'|path='$ROOT/dir.1/file.new'" | grep type=file || error "missing create dir.1/file.new"
+    egrep '^++' report.out | grep -v '+++' | grep -E "name='dir.new'|path='$ROOT/dir.new'" | grep type=dir || error "missing create dir.new"
     # rmd entries dir.1/b and dir.3
     nbrm=$(egrep -e '^--' report.out | grep -v -- '---' | wc -l)
     [ $nbrm  -eq 2 ] || error "$nbrm/2 removal"
@@ -3266,6 +3274,7 @@ function test_diff
         grep "^+[^ ]*"$(get_id "$ROOT/dir.2/f") report.out | grep stripe || error "missing stripe change $ROOT/dir.2/f"
     fi
 
+    # TODO check the content of the DB for scan and diff --apply
 }
 
 function test_completion
@@ -7626,7 +7635,9 @@ run_test 103c    test_acct_table acct_user.conf 5 "Acct table and triggers creat
 run_test 103d    test_acct_table acct_user_group.conf 5 "Acct table and triggers creation"
 run_test 104     test_size_updt test_updt.conf 1 "test size update"
 run_test 105     test_enoent test_pipeline.conf "readlog with continuous create/unlink"
-run_test 106     test_diff info_collect2.conf "rbh-diff"
+run_test 106a    test_diff info_collect2.conf "diff" "rbh-diff"
+run_test 106b    test_diff info_collect2.conf "diffapply" "rbh-diff --apply"
+run_test 106c    test_diff info_collect2.conf "scan" "robinhood --scan --diff"
 run_test 107a    test_completion test_completion.conf OK "scan completion command"
 run_test 107b    test_completion test_completion_KO.conf KO "bad scan completion command"
 run_test 108a    test_rename info_collect.conf scan "rename cases (scan)"

tests/posix_fs/2-run-tests.sh
@@ -1639,15 +1639,24 @@ function test_diff
     mv $ROOT/dir.2/d  $ROOT/dir.1/d     || error "mv"
     mv $ROOT/file $ROOT/fname           || error "rename"
 
-    echo "2-diff..."
-    $DIFF -f ./cfg/$config_file -l FULL > report.out 2> rh_report.log || error "performing diff"
+    # need 1s difference for md and name GC
+    sleep 1
+
+    echo "2-diff ($policy_str)..."
+    if [ "$flavor" = "diff" ]; then
+        $DIFF -f ./cfg/$config_file -l FULL > report.out 2> rh_report.log || error "performing diff"
+    elif [ "$flavor" = "diffapply" ]; then
+        $DIFF --apply=db -f ./cfg/$config_file -l FULL > report.out 2> rh_report.log || error "performing diff"
+    elif [ "$flavor" = "scan" ]; then
+        $RH -f ./cfg/$config_file -l FULL --scan --once --diff=all -L rh_report.log > report.out || error "performing scan+diff"
+    fi
 
     [ "$DEBUG" = "1" ] && cat report.out
 
     # must get:
     # new entries dir.1/file.new and dir.new
-    egrep '^++' report.out | grep -v '+++' | grep dir.1/file.new | grep type=file || error "missing create dir.1/file.new"
-    egrep '^++' report.out | grep -v '+++' | grep dir.new | grep type=dir || error "missing create dir.new"
+    egrep '^++' report.out | grep -v '+++' | grep -E "name='file.new'|path='$ROOT/dir.1/file.new'" | grep type=file || error "missing create dir.1/file.new"
+    egrep '^++' report.out | grep -v '+++' | grep -E "name='dir.new'|path='$ROOT/dir.new'" | grep type=dir || error "missing create dir.new"
     # rmd entries dir.1/b and dir.3
     nbrm=$(egrep '^--' report.out | grep -v -- '---' | wc -l)
     [ $nbrm  -eq 2 ] || error "$nbrm/2 removal"
@@ -1658,6 +1667,8 @@ function test_diff
     grep "^+[^ ]*"$(get_id "$ROOT/dir.1/c") report.out | grep size= || error "missing size change $ROOT/dir.1/c"
     grep "^+[^ ]*"$(get_id "$ROOT/dir.1/d") report.out | grep path= || error "missing path change $ROOT/dir.1/d"
     grep "^+[^ ]*"$(get_id "$ROOT/fname") report.out | grep path= || error "missing path change $ROOT/fname"
+
+    # TODO check the content of the DB for scan and diff --apply
 }
 
 function test_completion
@@ -1796,12 +1807,7 @@ function test_rename
         done
     fi
 
-    if [ "$flavor" = "partial" ] || [ "$flavor" = "partdiff" ]; then
-        # entries with no path may appear with partial scans
-        $REPORT -f ./cfg/$config_file --dump-all -q | grep -Ev " n/a$" > report.out
-    else
-        $REPORT -f ./cfg/$config_file --dump-all -q > report.out || error "$REPORT"
-    fi
+    $REPORT -f ./cfg/$config_file --dump-all -q > report.out || error "$REPORT"
     [ "$DEBUG" = "1" ] && cat report.out
 
     $FIND -f ./cfg/$config_file $ROOT -ls > find.out || error "$FIND"
@@ -1970,12 +1976,7 @@ function test_hardlinks
         done
     fi
 
-    if [ "$flavor" = "partial" ] || [ "$flavor" = "partdiff" ] ; then
-        # entries with no path may appear with partial scans
-        $REPORT -f ./cfg/$config_file --dump-all -q | grep -Ev " n/a$" > report.out
-    else
-        $REPORT -f ./cfg/$config_file --dump-all -q > report.out || error "$REPORT"
-    fi
+    $REPORT -f ./cfg/$config_file --dump-all -q > report.out || error "$REPORT"
     [ "$DEBUG" = "1" ] && cat report.out
 
     $FIND -f ./cfg/$config_file $ROOT -ls > find.out || error "$FIND"
@@ -3931,7 +3932,10 @@ run_test 103a    test_acct_table common.conf 5 "Acct table and triggers creation
 run_test 103b    test_acct_table acct_group.conf 5 "Acct table and triggers creation"
 run_test 103c    test_acct_table acct_user.conf 5 "Acct table and triggers creation"
 run_test 103d    test_acct_table acct_user_group.conf 5 "Acct table and triggers creation"
-run_test 106     test_diff info_collect2.conf "rbh-diff"
+
+run_test 106a    test_diff info_collect2.conf "diff" "rbh-diff"
+run_test 106b    test_diff info_collect2.conf "diffapply" "rbh-diff --apply"
+run_test 106c    test_diff info_collect2.conf "scan" "robinhood --scan --diff"
 run_test 107a    test_completion test_completion.conf OK "scan completion command"
 run_test 107b    test_completion test_completion_KO.conf KO "bad scan completion command"
 run_test 108a    test_rename info_collect.conf scan "rename cases (scan)"

src/include/list_mgr.h
@@ -987,8 +987,8 @@ void           ListMgr_MergeAttrSets( attr_set_t * p_target_attrset,
                                       attr_set_t * p_source_attrset,
                                       int update );
 
-/** only keep in target attrset the fields that are different from source */
-void ListMgr_KeepDiff(attr_set_t * p_tgt, const attr_set_t * p_src);
+/** return the mask of attributes that differ */
+int ListMgr_WhatDiff(const attr_set_t * p_tgt, const attr_set_t * p_src);
 
 /**
  * Generate fields automatically from already existing fields,

src/list_mgr/listmgr_common.c
@@ -1279,16 +1279,17 @@ void ListMgr_FreeAttrs( attr_set_t * p_set )
 #endif
 }
 
-/** only keep the differences in target attrset */
-void ListMgr_KeepDiff(attr_set_t * p_tgt, const attr_set_t * p_src)
+/** return the mask of attributes that differ */
+int ListMgr_WhatDiff(const attr_set_t * p_tgt, const attr_set_t * p_src)
 {
     int            i;
-    int            mask = 1;
-    int diff_mask = p_tgt->attr_mask & p_src->attr_mask;
+    int            bit = 1;
+    int common_mask = p_tgt->attr_mask & p_src->attr_mask;
+    int diff_mask = 0;
 
-    for ( i = 0; i < ATTR_COUNT; i++, mask <<= 1 )
+    for ( i = 0; i < ATTR_COUNT; i++, bit <<= 1 )
     {
-        if (mask & diff_mask)
+        if (bit & common_mask)
         {
             int is_diff = 0;
             if ( !is_stripe_field( i ) )
@@ -1299,20 +1300,20 @@ void ListMgr_KeepDiff(attr_set_t * p_tgt, const attr_set_t * p_src)
                                 field_infos[i].offset),
                            ((char *)&p_tgt->attr_values +
                                 field_infos[i].offset));
-                if (!is_diff)
-                    p_tgt->attr_mask &= ~mask;
+                if (is_diff)
+                    diff_mask |= bit;
             }
 #ifdef _LUSTRE
             else if ( field_infos[i].db_type == DB_STRIPE_INFO )
             {
                 if ((ATTR(p_tgt, stripe_info).stripe_size
-                        == ATTR(p_src, stripe_info).stripe_size)
-                    && (ATTR(p_tgt, stripe_info).stripe_count
-                        == ATTR(p_src, stripe_info).stripe_count)
-                    && (!strcmp(ATTR(p_tgt, stripe_info).pool_name,
-                            ATTR(p_src, stripe_info).pool_name)))
+                        != ATTR(p_src, stripe_info).stripe_size)
+                    || (ATTR(p_tgt, stripe_info).stripe_count
+                        != ATTR(p_src, stripe_info).stripe_count)
+                    || (strcmp(ATTR(p_tgt, stripe_info).pool_name,
+                           ATTR(p_src, stripe_info).pool_name) != 0))
                 {
-                    p_tgt->attr_mask &= ~mask;
+                    diff_mask |= bit;
                 }
             }
             else if ( field_infos[i].db_type == DB_STRIPE_ITEMS )
@@ -1342,13 +1343,13 @@ void ListMgr_KeepDiff(attr_set_t * p_tgt, const attr_set_t * p_src)
                         }
                     }
                 }
-                if (!is_diff)
-                    p_tgt->attr_mask &= ~mask;
+                if (is_diff)
+                     diff_mask |= bit;
             }
 #endif
         }
     }
-    return;
+    return diff_mask;
 }
 
 /** Convert a set notation (eg. "3,5-8,12") to a list of values

src/todo
@@ -1,8 +1,7 @@
-- partial scan: update directories between root and partial dir
 - restore bulk search in rbh-find as a new option
 - support ost sets in rbh_find
 - non reg test for OST sets
-- backport recent modes to std_pipeline to diff pipeline.
+- backport recent modes in std_pipeline to diff pipeline.
 - hardlink management in policies, recovery, etc.
 - check how mknode'd entries and handled by backup mode and rbh-diff
 - fix name update warnings

commit 750a98500079e7b8eccd922489ebe7ef8daa263b

pipeline: fixed regression introduced by e214f1f1.
Most file attributes where not updated when performing an update scan,
or even when processing changelogs...

src/entry_processor/diff_pipeline.c
@@ -766,8 +766,8 @@ int EntryProc_report_diff( struct entry_proc_op_t *p_op, lmgr_t * lmgr )
 #endif
             to_keep |= (ATTR_MASK_md_update | ATTR_MASK_path_update);
 
-        /* remove other unchanged attrs */
-        p_op->fs_attrs.attr_mask &= (diff_mask | to_keep);
+        /* remove other unchanged attrs or attrs not in db mask */
+        p_op->fs_attrs.attr_mask &= (diff_mask | to_keep | ~p_op->db_attrs.attr_mask);
 
         /* nothing changed => noop */
         if (p_op->fs_attrs.attr_mask == 0)

src/entry_processor/std_pipeline.c
@@ -1888,8 +1888,8 @@ int EntryProc_db_apply( struct entry_proc_op_t *p_op, lmgr_t * lmgr )
 #endif
             to_keep |= (ATTR_MASK_md_update | ATTR_MASK_path_update);
 
-        /* remove other unchanged attrs */
-        p_op->fs_attrs.attr_mask &= (diff_mask | to_keep);
+        /* remove other unchanged attrs + attrs not in db mask */
+        p_op->fs_attrs.attr_mask &= (diff_mask | to_keep | ~p_op->db_attrs.attr_mask);
 
         /* SQL req optimizations:
          * if update policy == always and fileclass is not changed,

tests/lustre_fs/2-run-tests.sh
@@ -6225,7 +6225,7 @@ function test_purge
 	    fi
     fi
 
-	echo "Reading changelogs and Applying purge policy..."
+	echo "Scan and apply purge policy..."
 	$RH -f ./cfg/$config_file --scan  $purgeOpt --once -l DEBUG -L rh_purge.log
 
 	nbError=0