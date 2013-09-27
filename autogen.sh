57bc34d0fcf18c8949d67a20c44dd3c146c78631
Thomas Leibovici <thomas.leibovici@cea.fr>
2013-09-27 10:20:41 +0200

    repository: autogen.sh installs git prepare-commit hook.
    Add comments into git commit messages:
    - template commit message format
    - warnings about code syntax
    - diff output

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 autoreconf --install
+
+wdir=$(dirname $(readlink -m $0))
+# if git prepare commit hook is a file, rename it to .orig
+# and replace it with a link to the project hook
+if [ -f $wdir/.git/hooks/prepare-commit-msg ]; then
+    echo "installing git prepare-commit hook"
+    mv -f $wdir/.git/hooks/prepare-commit-msg $wdir/.git/hooks/prepare-commit-msg.save || exit 1
+    ln -s '../../scripts/git_prepare_hook' $wdir/.git/hooks/prepare-commit-msg || exit 1
+fi
