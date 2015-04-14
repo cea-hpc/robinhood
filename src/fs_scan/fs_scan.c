49ffc45c63b8b149fa069a5921770b5fe54758dc
Thomas Leibovici <thomas.leibovici@cea.fr>
2015-04-14 13:05:24 +0200

    FS scan: fix invalid termination status when the scan is aborted.
    When the scan was aborted, the FS scan was reported as "complete"
    and a garbage collection of old files was performed.
    Fix this to end the scan ASAP when catching ctrl+c.
    
    Change-Id: Ibb28f3b43d1a104aa450f206e84bce96879cd7ba

--- src/fs_scan/fs_scan.c
+++ src/fs_scan/fs_scan.c
@@ -339,7 +339,7 @@ static int TerminateScan(int scan_complete, time_t end)
     }
 
     /* if no entry was listed or scan is faulty, don't flush the pipeline. */
-    if ((scan_count(true) > 0) || scan_complete)
+    if ((scan_count(true) > 0) && scan_complete)
     {
         entry_proc_op_t *op;
 
@@ -527,11 +527,14 @@ static int RecursiveTaskTermination(thread_scan_info_t *p_info,
                     err_count += thread_list[i].entries_errors;
                 }
 
-                DisplayLog( LVL_MAJOR, FSSCAN_TAG,
-                            "Full scan of %s completed, %u entries found (%u errors). Duration = %ld.%02lds",
-                            partial_scan_root?partial_scan_root:global_config.fs_path,
-                            count, err_count, duree_precise.tv_sec,
-                            duree_precise.tv_usec/10000 );
+                if (bool_scan_complete)
+                    DisplayLog(LVL_MAJOR, FSSCAN_TAG,
+                               "%s of %s %s, %u entries found (%u errors). Duration = %ld.%02lds",
+                               bool_scan_complete?"Full scan":"Scan",
+                               partial_scan_root?partial_scan_root:global_config.fs_path,
+                               bool_scan_complete?"completed":"aborted",
+                               count, err_count, duree_precise.tv_sec,
+                               duree_precise.tv_usec/10000);
 
                 DisplayLog( LVL_EVENT, FSSCAN_TAG, "Flushing pipeline..." );
 
@@ -1435,7 +1438,8 @@ static void   *Thr_scan(void *arg_thread)
         }
 
         /* terminate processing of current task */
-        rc = RecursiveTaskTermination(p_info, p_task, (task_rc == 0));
+        rc = RecursiveTaskTermination(p_info, p_task, (task_rc == 0)
+                                                      && !p_info->force_stop);
         if (rc)
         {
             DisplayLog(LVL_CRIT, FSSCAN_TAG,
