a8f454c489aaa5db8659c15adba1b7e950469035
Thomas Leibovici <thomas.leibovici@cea.fr>
2015-09-24 20:13:39 +0200

    Update config parser to Bison 3.0.2.
    Change-Id: I0b0d84a04c6579defc3b109f1de7a2c902a49e81

--- src/cfg_parsing/conf_yacc.h
+++ src/cfg_parsing/conf_yacc.h
@@ -1,21 +1,19 @@
+/* A Bison parser, made by GNU Bison 3.0.2.  */
 
-/* A Bison parser, made by GNU Bison 2.4.1.  */
+/* Bison interface for Yacc-like parsers in C
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
-   
-      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
-   
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
@@ -28,42 +26,50 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-   
+
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
+#ifndef YY_YY_CONF_YACC_H_INCLUDED
+# define YY_YY_CONF_YACC_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
 
-/* Tokens.  */
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     _ERROR_ = 258,
-     BEGIN_BLOCK = 259,
-     END_BLOCK = 260,
-     END_AFFECT = 261,
-     BEGIN_SUB_BLOCK = 262,
-     END_SUB_BLOCK = 263,
-     BEGIN_PARENTHESIS = 264,
-     END_PARENTHESIS = 265,
-     VALUE_SEPARATOR = 266,
-     AFFECT = 267,
-     EQUAL = 268,
-     DIFF = 269,
-     GT = 270,
-     GT_EQ = 271,
-     LT = 272,
-     LT_EQ = 273,
-     AND = 274,
-     OR = 275,
-     NOT = 276,
-     UNION = 277,
-     INTER = 278,
-     IDENTIFIER = 279,
-     NON_IDENTIFIER_VALUE = 280,
-     ENV_VAR = 281
-   };
+  enum yytokentype
+  {
+    _ERROR_ = 258,
+    BEGIN_BLOCK = 259,
+    END_BLOCK = 260,
+    END_AFFECT = 261,
+    BEGIN_SUB_BLOCK = 262,
+    END_SUB_BLOCK = 263,
+    BEGIN_PARENTHESIS = 264,
+    END_PARENTHESIS = 265,
+    VALUE_SEPARATOR = 266,
+    AFFECT = 267,
+    EQUAL = 268,
+    DIFF = 269,
+    GT = 270,
+    GT_EQ = 271,
+    LT = 272,
+    LT_EQ = 273,
+    AND = 274,
+    OR = 275,
+    NOT = 276,
+    UNION = 277,
+    INTER = 278,
+    IDENTIFIER = 279,
+    NON_IDENTIFIER_VALUE = 280,
+    ENV_VAR = 281
+  };
 #endif
 /* Tokens.  */
 #define _ERROR_ 258
@@ -91,31 +97,27 @@
 #define NON_IDENTIFIER_VALUE 280
 #define ENV_VAR 281
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
+typedef union YYSTYPE YYSTYPE;
+union YYSTYPE
 {
-
-/* Line 1676 of yacc.c  */
-#line 52 "conf_yacc.y"
+#line 52 "conf_yacc.y" /* yacc.c:1909  */
 
     char         str_val[MAXSTRLEN];
     list_items              *  list;
     generic_item            *  item;
     arg_list_t	            *  arg_list;
 
-
-
-/* Line 1676 of yacc.c  */
-#line 113 "conf_yacc.h"
-} YYSTYPE;
+#line 113 "conf_yacc.h" /* yacc.c:1909  */
+};
 # define YYSTYPE_IS_TRIVIAL 1
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 #endif
 
+
 extern YYSTYPE yylval;
 
+int yyparse (void);
 
+#endif /* !YY_YY_CONF_YACC_H_INCLUDED  */
