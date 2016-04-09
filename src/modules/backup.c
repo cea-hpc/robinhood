81f4489b6346767103a367d94ece9022cc22e06b
Thomas Leibovici <thomas.leibovici@cea.fr>
2016-04-09 17:36:08 +0200

    common: deal with multiple filesystems mounted on the same path.
    When several filesystems are mounted on the same path,
    only the last matching filesystem in mtab must be taken into account.
    
    Other changes in this patch:
    * less stack usage
    * improved function style
    * lower cased function name
    * function returns -errno
    
    Change-Id: I26d1489f96e6555236a7dc5b02670bc3e85d5fb8

--- src/modules/backup.c
+++ src/modules/backup.c
@@ -375,7 +375,7 @@ static int backup_init(struct sm_instance *smi, run_flags_t flags)
     rc = CheckFSInfo(config.root, config.mnt_type, &backend_dev,
                      backend_name, config.check_mounted, false);
     if (rc)
-        return -rc;
+        return rc;
 
     return 0;
 }
