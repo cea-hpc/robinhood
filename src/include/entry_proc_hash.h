c8d769dbdd5e309776085e93781a322971cd8aa5
Henri Doreau <henri.doreau@cea.fr>
2016-05-09 14:25:10 +0200

    Speedup entries hashing
    Replaced custom hashing functions by more standard mechanisms based
    on GLib string hash function and Murmur3 u64 finalizer.
    
    This code section was identified as a performance hot point during
    profiling. Raw functions are several order of magnitude faster
    (x600 speedup measured) and gprof confirms that this is no longer
    the bottleneck.
    
    Signed-off-by: Henri Doreau <henri.doreau@cea.fr>
    Change-Id: I5d07ad3270cd6923a57343707536192fd1af9327

--- src/include/entry_proc_hash.h
+++ src/include/entry_proc_hash.h
@@ -15,21 +15,22 @@
 /**
  * Misc tools for managing entry processor pipeline
  */
+
 #ifndef _ENTRY_PROC_HASH_H
 #define _ENTRY_PROC_HASH_H
 
+#include <glib.h>
+
 /* A hash table slot. */
-struct id_hash_slot
-{
-    pthread_mutex_t lock;
+struct id_hash_slot {
+    pthread_mutex_t     lock;
     struct rh_list_head list;      /* list of ops */
-    unsigned int count;
+    unsigned int        count;
 };
 
 /* A hash table. */
-struct id_hash
-{
-    unsigned int hash_size;
+struct id_hash {
+    unsigned int        hash_size;
 	struct id_hash_slot slot[];
 };
 
@@ -46,32 +47,27 @@ void id_hash_stats(struct id_hash *id_hash, const char *log_str);
 /* dump all values in the hash */
 void id_hash_dump(struct id_hash *id_hash, bool parent);
 
+
 /**
- * @TODO use better hash functions:
- *       - http://code.google.com/p/smhasher/wiki/MurmurHash3
- *    or - http://burtleburtle.net/bob/c/lookup3.c
+ * Murmur3 uint64 finalizer
+ * from: https://github.com/aappleby/smhasher/blob/master/src/MurmurHash3.cpp
  */
+static inline uint64_t __hash64(uint64_t k)
+{
+    k ^= k >> 33;
+    k *= 0xff51afd7ed558ccdLLU;
+    k ^= k >> 33;
+    k *= 0xc4ceb9fe1a85ec53LLU;
+    k ^= k >> 33;
+    return k;
+}
+
 static inline uint64_t id_hash64(const entry_id_t * p_id)
 {
 #ifdef FID_PK
-	uint64_t       val = 1;
-    char          *buffer;
-    unsigned int   index;
-
-	buffer = (char *) &(p_id->f_seq);
-
-	for (index = 0; index < sizeof(p_id->f_seq); index++)
-		val = (val << 5) - val + (unsigned int)(buffer[index]);
-
-	buffer = (char *) &(p_id->f_oid);
-
-	for (index = 0; index < sizeof(p_id->f_oid); index++)
-		val = (val << 5) - val + (unsigned int)(buffer[index]);
-
-    return val;
+    return __hash64(p_id->f_seq ^ p_id->f_oid);
 #else
-    /* polynom of prime numbers */
-	return 1873 * p_id->fs_key + 3511 * p_id->inode + 10267;
+    return __hash64(p_id->fs_key ^ p_id->inode);
 #endif
 }
 
@@ -83,26 +79,20 @@ static inline unsigned int hash_id(const entry_id_t * p_id, unsigned int modulo)
 static inline unsigned int hash_name(const entry_id_t * p_id,
                                      const char *name, unsigned int modulo)
 {
-    uint64_t val = id_hash64(p_id);
-    const char *c;
-
-    for (c = name; *c != '\0'; c++)
-        val = (val << 5) - val + (unsigned int)(*c);
-
-    return val % modulo;
+    return (id_hash64(p_id) ^ g_str_hash(name)) % modulo;
 }
 
 /* return a slot pointer. */
-static inline struct id_hash_slot * get_hash_slot( struct id_hash * id_hash,
-												   const entry_id_t * p_id)
+static inline struct id_hash_slot *get_hash_slot(struct id_hash * id_hash,
+                                                 const entry_id_t * p_id)
 {
-	return &id_hash->slot[hash_id( p_id, id_hash->hash_size )];
+	return &id_hash->slot[hash_id(p_id, id_hash->hash_size)];
 }
 
 /* return a slot pointer. */
 static inline struct id_hash_slot *get_name_hash_slot(struct id_hash *name_hash,
-                                                   const entry_id_t *parent_id,
-                                                   const char *name)
+                                                    const entry_id_t *parent_id,
+                                                    const char *name)
 {
     return &name_hash->slot[hash_name(parent_id, name, name_hash->hash_size)];
 }
