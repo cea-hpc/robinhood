74301a3f8b8c0f5158a8ecdad4b5a18b3a3bd630
Thomas Leibovici <thomas.leibovici@cea.fr>
2016-06-02 10:48:39 +0200

    (DUP) Use a constant for max user/group name length.
    Define a constant instead of a hardcoded value.
    
    Change-Id: I65c13063443b54afcb4f27893469f3b4448943ba

--- src/include/rbh_const.h
+++ src/include/rbh_const.h
@@ -6,6 +6,7 @@
 #define RBH_PATH_MAX    PATH_MAX
 #define RBH_NAME_MAX    256
 #define MAX_POOL_LEN    17     /* LOV_MAXPOOLNAME + 1 */
+#define RBH_LOGIN_MAX	128    /* user/group max name length */
 
 
 #endif
