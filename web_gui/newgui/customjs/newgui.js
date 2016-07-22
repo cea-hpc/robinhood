83ccfc22ba144adaa0e2213c23183f2aad4ae387
Jerome Andrieu <jerome.andrieu@cea.fr>
2016-07-22 08:37:57 +0200

    webgui: add newgui
    Change-Id: I2f67fedc4f6acabb775f9afaeaa178fb477f1de5

---
+++ web_gui/newgui/customjs/newgui.js
@@ -0,0 +1,242 @@
+/*
+ * Copyright (C) 2016 CEA/DAM
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the CeCILL License.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license (http://www.cecill.info) and that you
+ * accept its terms.
+ */
+
+/*************************
+ * Graph VAR
+ ************************/
+var GraphState="empty";
+var TableState="empty";
+var myChart;
+var GraphCTX;
+var lastGet="empty";
+//Global Graph
+//Chart.defaults.global.responsive = true;
+Chart.defaults.global.title.display = true;
+Chart.defaults.global.defaultFontSize = 13;
+
+Chart.pluginService.register({
+  beforeRender: function (chart) {
+    if (chart.config.options.showAllTooltips && (chart.config.type == "doughnut" || chart.config.type == "bar" || chart.config.type == "pie" )) {
+        // create an array of tooltips
+        // we can't use the chart tooltip because there is only one tooltip per chart
+        chart.pluginTooltips = [];
+        chart.config.data.datasets.forEach(function (dataset, i) {
+            chart.getDatasetMeta(i).data.forEach(function (sector, j) {
+                chart.pluginTooltips.push(new Chart.Tooltip({
+                    _chart: chart.chart,
+                    _chartInstance: chart,
+                    _data: chart.data,
+                    _options: chart.options.tooltips,
+                    _active: [sector]
+                }, chart));
+            });
+        });
+        // turn off normal tooltips
+        chart.options.tooltips.enabled = false;
+    }
+},
+  afterDraw: function (chart, easing) {
+    if (chart.config.options.showAllTooltips) {
+        // we don't want the permanent tooltips to animate, so don't do anything till the animation runs atleast once
+        if (!chart.allTooltipsOnce) {
+            if (easing !== 1)
+                return;
+            chart.allTooltipsOnce = true;
+        }
+
+        // turn on tooltips
+        chart.options.tooltips.enabled = true;
+        Chart.helpers.each(chart.pluginTooltips, function (tooltip) {
+            tooltip.initialize();
+            tooltip.update();
+            // we don't actually need this since we are not animating tooltips
+            tooltip.pivot();
+            tooltip.transition(easing).draw();
+        });
+        chart.options.tooltips.enabled = false;
+    }
+  }
+});
+
+function formatBytes(bytes,decimals) {
+   if(bytes == 0) return '0 Byte';
+   var k = 1024;
+   var dm = decimals + 1 || 3;
+   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+   var i = Math.floor(Math.log(bytes) / Math.log(k));
+   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+function formatCount(bytes,decimals) {
+   if(bytes == 0) return 'None';
+   var k = 1000;
+   var dm = decimals + 1 || 3;
+   var sizes = ['', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
+   var i = Math.floor(Math.log(bytes) / Math.log(k));
+   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+function getRGB(str){
+    var hash = $.md5(str);
+    var rgb = '#' + hash.substring(0,2) + hash.substring(2,4) + hash.substring(4,6);
+    return rgb;
+}
+
+function msg_warning(str){
+$('#messagebox').html('<div class="alert alert-warning">'+
+  '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' +
+  '<strong>Warning!</strong> ' +
+  str +
+  '</div>');
+}
+
+function msg_danger(str){
+$('#messagebox').html('<div class="alert alert-danger">'+
+  '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' +
+  '<strong>Danger!</strong> ' +
+  str +
+  '</div>');
+}
+
+
+function msg_clean() {
+  $('#messagebox').html('');
+}
+
+/*********************************
+ * Helper to check the API State
+ * and document state.
+ ********************************/
+$(function() {
+                $.ajax({
+url: "api/index.php?request=robinhood"
+}).then(function(data) {
+        $('#quote').append(data);
+        });
+		$.ajax({
+url: "api/index.php?request=native/vars"
+}).then(function(data) {
+	newdata = JSON.parse(data);
+	console.log(newdata)
+        $('#main_title').append(": "+newdata.FS_Path);
+	document.title = document.title + ": "+newdata.FS_Path;
+        });
+
+	});
+
+/****************************************
+ * Async. function which request graph
+ * and data, then update graph and table
+ **************************************/
+//GetGraph and Table Data async
+function GetGraph(item){
+
+        lastGet=item;
+
+        //Get filter
+        var queryString="FILTER:"
+        var myForm = document.getElementById("filterform");
+        for (var i = 0; i < myForm.elements.length; i++) {
+                if (myForm.elements[i].name.length>0)
+                {
+                        queryString = queryString + myForm.elements[i].name + "=" + myForm.elements[i].value + "!"
+                }
+        }
+
+
+        //Get the Graph data
+        $.ajax({
+url: "api/index.php?request=graph/" + item + "/" + queryString
+}).then(function(data) {
+
+        var options = {
+responsive : true,
+animation : false,
+showAllTooltips: true,
+defaultFontSize: 14,
+tooltips: {
+                enabled: true,
+                mode: 'single',
+                callbacks: {
+                    label: function(tooltipItems, data) {
+                        val = data.datasets[tooltipItems.datasetIndex].data[tooltipItems.index]
+                        type = data.datasets[tooltipItems.datasetIndex].unit
+                        if (type=="size") {
+                            return formatBytes(val,0);
+                        }
+                        if (type=="count") {
+                            return formatCount(val,0);
+                        }
+                    return val
+                    }
+                }}
+}
+//Convert string to json
+newdata = JSON.parse(data);
+
+if (newdata.limited) {
+	msg_warning("Partial result,  limited to "+newdata.limited+" entries by $MAX_ROWS (config.php). Please use filter !");
+} else {
+	msg_clean();
+}
+
+//Delete the old graph
+if (GraphState!="empty") {
+myChart.destroy();
+}
+
+GraphCTX = document.getElementById("ctx").getContext("2d");
+
+
+//Create the new graph
+myChart = new Chart(GraphCTX,{
+type: newdata.default_graph,
+data: newdata,
+options: options
+});
+
+GraphState=newdata.default_graph;
+
+});
+
+
+$.ajax({
+url: "api/index.php?request=data/" + item + "/" +queryString
+}).then(function(data) {
+        newdata = JSON.parse(data);
+        if (TableState!="empty"){
+            TableState.destroy();
+            $('#datalist').empty();
+        }
+
+        TableState = $('#datalist').DataTable( {
+            destroy: true,
+            clear: true,
+            data: newdata.datasets,
+            columns: newdata.columns,
+            columnDefs: newdata.columnsDefs
+        } );
+
+});
+
+
+}
+
+/*********************************
+ * Clean the filter form
+ ********************************/
+
+function CleanForm() {
+	var myForm = document.getElementById("filterform");
+	for (var i = 0; i < myForm.elements.length; i++) {
+		myForm.elements[i].value = ""
+	}
+}
---
+++ web_gui/newgui/index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<?php
+/*
+ * Copyright (C) 2016 CEA/DAM
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the CeCILL License.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license (http://www.cecill.info) and that you
+ * accept its terms.
+ */
+
+include "config.php";
+include "common.php";
+
+?>
+
+<?php
+/*******************************************************
+*                   HEADER                             *
+*******************************************************/
+?>
+<html lang="en">
+<head>
+<title>Robinhood Report</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/dataTables.bootstrap.css">
+<link rel="stylesheet" href="css/dashboard.css">
+<script src="js/jquery-2.2.4.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/Chart.bundle.js"></script>
+<script src="js/jquery.dataTables.js"></script>
+<script src="js/filesize.dataTables.js"></script>
+<script src="customjs/newgui.js"></script>
+
+</head>
+
+<?php
+/*******************************************************
+*                   BODY                               *
+*******************************************************/
+?>
+<body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#" id="main_title">Robinhood Policy Engine</a>
+
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="https://github.com/cea-hpc/robinhood/wiki/Documentation">Help</a></li>
+          </ul>
+	<i><p id="quote" class="navbar-text"></p></i>
+
+        </div>
+      </div>
+    </nav>
+
+    <div class="container-fluid">
+      <div  class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+	 <div class="nav nav-sidebar">
+	  <a class="navbar-brand" href="http://robinhood.sf.net"><img src="images/logo_rh_sf.gif" ></a>
+	  <a class="navbar-brand" href="http://www-hpc.cea.fr/index-en.htm"><img src="images/logoCEA20.jpg" height="64"></a>
+	</div><br><br>
+          <ul class="nav nav-sidebar">
+<?php
+
+/****************************************
+ *       LEFT MENU BUTTONS               *
+ ****************************************/
+
+$fields = get_acct_columns();
+foreach ($fields as $field) {
+        echo '<li><a href="#" onclick="GetGraph(\''.$field.'\')">'.l($field).'</a></li>';
+}
+
+echo '<li><a href="#"  onclick="GetGraph(\'Files\')">Files</a></li>';
+?>
+
+          </ul>
+<?php
+/****************************************
+ *                 FILTER               *
+ ****************************************/
+?>
+
+	<form id="filterform" name="filterform">
+	<fieldset class="form-group">
+	<label for="formUID">Filter</label>
+	<input type="text" class="form-control" id="formUID" name="uid" placeholder="UID">
+	</fieldset>
+	<fieldset class="form-group">
+	<input type="text" class="form-control" id="formGID" name="gid" placeholder="GID">
+	</fieldset>
+	<fieldset class="form-group">
+	<input type="text" class="form-control" id="formFilename" name="filename" placeholder="Filename">
+	</fieldset>
+	<button type="button" class="btn btn-primary" onclick="GetGraph(lastGet)">Filter</button>
+	<button type="button" class="btn btn-primary" onclick="CleanForm();GetGraph(lastGet)">Clean</button>
+	</form>
+
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+	   <div id="messagebox"></div>
+	    <canvas id="ctx"></canvas>
+	    <table id="datalist" class="table table-striped table-bordered" width="100%"></table>
+
+	  </div>
+
+        </div>
+      </div>
+    </div>
+
+
+
+<?php
+$permission = getFilePermission("config.php");
+if ($permission != "700") {
+echo "<script>$(msg_danger(\"Bad permission on config.php ($permission) shoud be 700 .\"))</script>";
+}
+?>
+
+</body>
+</html>
+
