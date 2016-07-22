13f1c7e09128729438c7f66b13c5c30f81081b32
Jerome Andrieu <jerome.andrieu@cea.fr>
2016-07-22 08:37:57 +0200

    webgui: add newgui (working) draft
    Change-Id: I2f67fedc4f6acabb775f9afaeaa178fb477f1de5

---
+++ web_gui/newgui/js/filesize.dataTables.js
@@ -0,0 +1,23 @@
+jQuery.fn.dataTable.ext.type.order['file-size-pre'] = function ( data ) {
+    var matches = data.match( /^(\d+(?:\.\d+)?)\s*([a-z]+)/i );
+    var multipliers = {
+        b:  1,
+        kb: 1000,
+        kib: 1024,
+        mb: 1000000,
+        mib: 1048576,
+        gb: 1000000000,
+        gib: 1073741824,
+        tb: 1000000000000,
+        tib: 1099511627776,
+        pb: 1000000000000000,
+        pib: 1125899906842624
+    };
+
+    if (matches) {
+        var multiplier = multipliers[matches[2].toLowerCase()];
+        return parseFloat( matches[1] ) * multiplier;
+    } else {
+        return -1;
+    };
+};
---
+++ web_gui/newgui/lang/sys.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Copyright (C) 2016 CEA/DAM
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the CeCILL License.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license (http://www.cecill.info) and that you
+ * accept its terms.
+ */
+
+
+$lang = array();
+
+$lang['sz0'] = "Null";
+$lang['sz1'] = "1o-32o";
+$lang['sz32'] = "32o-1K";
+$lang['sz1K'] = "1K-32K";
+$lang['sz32K'] = "32K-1M";
+$lang['sz1M'] = "1M-32M";
+$lang['sz32M'] = "32M-1G";
+$lang['sz1G'] = "1G-32G";
+$lang['sz32G'] = "32G-1T";
+$lang['sz1T'] = "1T";
+?>
