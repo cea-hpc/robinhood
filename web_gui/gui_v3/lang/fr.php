8f104ab1caa56ceecc2aaa117601317eb39ca300
Jerome Andrieu <jerome.andrieu@cea.fr>
2016-07-22 08:37:57 +0200

    webgui: replace the old web gui
    Change-Id: I2f67fedc4f6acabb775f9afaeaa178fb477f1de5

---
+++ web_gui/gui_v3/lang/fr.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Copyright (C) 2016 CEA/DAM
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the CeCILL License.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license (http://www.cecill.info) and that you
+ * accept its terms.
+ */
+
+
+$lang['uid'] = "Proprietaire";
+$lang['gid'] = "Groupe";
+$lang['lhsm_status'] = "Status LHSM";
+$lang['checksum_status'] = "Status Checksum";
+
+?>
