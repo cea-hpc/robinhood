f5e76d63bb0327d0d9520841773ff1bfc93f3082
Richard <radamca@gsi.de>
2016-10-27 17:35:29 +0200

    gsihpc.js added

---
+++ web_gui/gui_v3/customjs/gsihpc.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+
+
+  $('#filterform input').on('keypress', function(event){
+    if(event.which==13 && !event.shiftKey){
+      GetGraph(lastGet);
+    }
+  });
+});
