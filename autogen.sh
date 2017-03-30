b0ae1bdb49438cd4614467074a345d70137a6928
Thomas Leibovici <thomas.leibovici@cea.fr>
2017-03-30 09:56:45 +0200

    git: display checkpatch warnings in commit message comments
    Display the output of checkpatch in the comments
    displayed below git commit message.
    
    Change-Id: I31265807a3316641d88a320f6817cd63c1ad3c01

--- autogen.sh
+++ autogen.sh
@@ -4,13 +4,13 @@ autoreconf --install
 wdir=$(dirname $(readlink -m "$0"))
 # if git prepare commit hook is a file, rename it to .orig
 # and replace it with a link to the project hook
-if [ -f "$wdir/.git/hooks/prepare-commit-msg" ]; then
+if [ -d "$wdir/.git/hooks" ]; then
     echo "installing git prepare-commit hook"
     mv -f "$wdir/.git/hooks/prepare-commit-msg" "$wdir/.git/hooks/prepare-commit-msg.save" || exit 1
     ln -s '../../scripts/git_prepare_hook' "$wdir/.git/hooks/prepare-commit-msg" || exit 1
 fi
 
-if [ -f "$wdir/.git/hooks/pre-commit" ]; then
+if [ -d "$wdir/.git/hooks" ]; then
     echo "installing git pre-commit hook"
     mv -f "$wdir/.git/hooks/pre-commit" "$wdir/.git/hooks/pre-commit.save" || exit 1
     ln -s '../../scripts/pre-commit' "$wdir/.git/hooks/pre-commit" || exit 1
