c6e35825b4b208285ca765c3160437d284fb989d
Thomas Leibovici <thomas.leibovici@cea.fr>
2017-04-06 15:21:23 +0200

    lmgr: add functions description
    Add doxygen description for function add_source_fields_for_gen()
    and generate_fields() (internal list_mgr functions).
    
    Change-Id: I258269fac705f207ca6f4b10b704d7dd2ad83205

--- src/list_mgr/listmgr_common.h
+++ src/list_mgr/listmgr_common.h
@@ -458,8 +458,18 @@ typedef enum {
     SUBTRACT
 } operation_type;
 
-void           add_source_fields_for_gen(uint32_t *std_mask);
-void           generate_fields( attr_set_t * p_set );
+/**
+ * Add needed source fields to build generated fields.
+ * @param[in,out] std_mask   Mask of attributes where the source fields bits are
+ *                           added.
+ */
+void  add_source_fields_for_gen(uint32_t *std_mask);
+/**
+ * Generate fields in the given attr_set.
+ * @param[in,out] p_set  Source fields are read from this attribute set, and
+ *                       generated fields are written to it.
+ */
+void  generate_fields(attr_set_t *p_set);
 
 int parse_entry_id(lmgr_t *p_mgr, const char *str, PK_PARG_T p_pk, entry_id_t *p_id);
 
