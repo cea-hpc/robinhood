cea-hpc/robinhood

commit eaaa8b2b500c27a73edb80c5ad06d350893de857

lmgr: GC at end of scan may fail if removed files have 2 parents.
one_path and this_path functions can fail during GC because removed files
have 2 possible parents.
testcase:
- run FS scan
- rename a directory
- remove a file within renamed dir
- run FS scan

Fix proposed by Davide Tacchella <dtacchella@cray.com>.

Change-Id: I8f109b2530676eef1bedb306ee92e89efd1b0863

src/list_mgr/listmgr_init.c
@@ -2216,7 +2216,7 @@ free_str:
 #define VERSION_VAR_FUNC    "VersionFunctionSet"
 #define VERSION_VAR_TRIG    "VersionTriggerSet"
 
-#define FUNCTIONSET_VERSION    "1.4"
+#define FUNCTIONSET_VERSION    "1.5"
 #define TRIGGERSET_VERSION     "1.3"
 
 static int check_functions_version(db_conn_t *conn)
@@ -2727,9 +2727,12 @@ static int create_func_onepath(db_conn_t *pconn)
             " DECLARE n VARBINARY(%u) DEFAULT NULL;"
             // returns path when parent is not found (NULL if id is not found)
             " DECLARE EXIT HANDLER FOR NOT FOUND RETURN CONCAT(pid,'/',p);"
-            " SELECT parent_id, name INTO pid, p from NAMES where id=param LIMIT 1;"
+            " SELECT parent_id, name INTO pid, p from NAMES WHERE id=param"
+                // limit result to newest path only
+                " ORDER BY path_update DESC LIMIT 1;"
             " LOOP"
-                " SELECT parent_id, name INTO pid, n from NAMES where id=pid ;"
+                " SELECT parent_id, name INTO pid, n from NAMES WHERE id=pid"
+                    " ORDER BY path_update DESC LIMIT 1;"
                 " SELECT CONCAT( n, '/', p) INTO p;"
             " END LOOP;"
         " END",
@@ -2785,7 +2788,9 @@ static int create_func_thispath(db_conn_t *pconn)
             " SET pid=pid_arg;"
             " SET p=n_arg;"
             " LOOP"
-                " SELECT parent_id, name INTO pid, n from NAMES where id=pid ;"
+                " SELECT parent_id, name INTO pid, n from NAMES WHERE id=pid"
+                    // limit result to newest path only
+                    " ORDER BY path_update DESC LIMIT 1;"
                 " SELECT CONCAT( n, '/', p) INTO p;"
             " END LOOP;"
         " END",

commit 980639ced4b4b016ca3d65379522930b98ccaf5c

listmgr: drop path ordering (query optimization)
Ordering paths by last update time resulted in an expensive execution path
in the database, even for sorting a single entry most of the time.
Dropping this unnecessary ordering results in lower CPU usage (-50%)
and increased database performance.

This performance regression was intruduced by patch
eaaa8b2b500c27a73edb80c5ad06d350893de857.
Kept the "limit 1" statement of this patch, but drop the "ORDER BY
path_update".

Change-Id: I9748aeafc87d10290aeccfc1a957884602c20bbb

src/list_mgr/listmgr_init.c
@@ -2645,11 +2645,11 @@ static int create_func_onepath(db_conn_t *pconn, bool *affects_trig)
                        (NULL if id is not found) */
                     " DECLARE EXIT HANDLER FOR NOT FOUND RETURN CONCAT(pid,'/',p);"
                     " SELECT parent_id, name INTO pid, p from NAMES WHERE id=param"
-                    /* limit result to newest path only */
-                    " ORDER BY path_update DESC LIMIT 1;"
+                    /* limit result to 1 path only */
+                    " LIMIT 1;"
                     " LOOP"
                     " SELECT parent_id, name INTO pid, n from NAMES WHERE id=pid"
-                    " ORDER BY path_update DESC LIMIT 1;"
+                    " LIMIT 1;"
                     " SELECT CONCAT( n, '/', p) INTO p;" " END LOOP;" " END",
                     /* size of fullpath */
                     field_infos[ATTR_INDEX_fullpath].db_type_size,
@@ -2710,8 +2710,8 @@ static int create_func_thispath(db_conn_t *pconn, bool *affects_trig)
                 " SET p=n_arg;"
                 " LOOP"
                 " SELECT parent_id, name INTO pid, n from NAMES WHERE id=pid"
-                // limit result to newest path only
-                " ORDER BY path_update DESC LIMIT 1;"
+                /* limit result to 1 path only */
+                " LIMIT 1;"
                 " SELECT CONCAT( n, '/', p) INTO p;" " END LOOP;" " END",
                 /* size of name */
                 field_infos[ATTR_INDEX_name].db_type_size,