f5940680258a324582693c8d0ea5310e4aaed1bf
Jerome Andrieu <jerome.andrieu@cea.fr>
2017-08-28 16:41:38 +0200

    web_ui: new features
    Add plugins:
        -stackgraph (group small values)
        -fsinfo (display robinhood vars)
        -browser (browse the filesystem)
        -colorgraph (change color set)
        -ldapauth (use ldap for data access)
        -console (run custom query from ui)
    
    Allow users to request a specific page and filters
        by settings parameter in the url
    Improve UI
    Add native/files API request
    Upgrade libs
    
    Change-Id: Ib1cab0c48a2d39e8b590a00a1e54cac79b230b7d

--- web_gui/gui_v3/api/index.php
+++ web_gui/gui_v3/api/index.php
@@ -12,18 +12,21 @@
 
 require_once("robinhood.php");
 require_once("../common.php");
+require_once("../plugin.php");
 
 if (!check_access("api-ro"))
-        return "Permission denied";
+    return "Permission denied";
 
 if (!array_key_exists('request', $_REQUEST)) {
     $_REQUEST['request']="";
 }
 
 try {
-        $API = new MyAPI($_REQUEST['request']);
-        echo $API->processAPI();
+    plugins_call("api_preprocess", $_REQUEST['request']);
+    $API = new MyAPI($_REQUEST['request']);
+    echo $API->processAPI();
 } catch (Exception $e) {
-        echo json_encode(Array('error' => $e->getMessage()));
+    echo json_encode(Array('error' => $e->getMessage()));
 }
+
 ?>
---
+++ web_gui/gui_v3/plugins/browser/script.js
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2016-2017 CEA/DAM
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the CeCILL License.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license (http://www.cecill.info) and that you
+ * accept its terms.
+ */
+
+
+/***********************************
+ * Build table and get data from API
+ * Use lazyload
+ **********************************/
+function browser_GetInfo(){
+
+        /* Create the table template */
+        document.getElementById("main_content").innerHTML = `
+                <h1>Browser</h1>
+                <div id="tree"></div>
+                `;
+
+
+        /* Retrieve the fid of the root */
+        $.ajax({
+                url: "api/index.php?request=native/vars"
+        }).then(function(data) {
+                var RootId=data['RootId'];
+                var tree = [{
+                        text: "/",
+                nodes: [],
+                id: data['RootId'],
+                lazyLoad: true,
+
+                }];
+                $('#tree').treeview({
+                        data: tree,
+                        showTags: true,
+                        lazyLoad: function(node, loader) {
+
+                                $.ajax({
+                                        url: "api/index.php?request=native/files/parent_id.filter/" + node.id
+                                }).then(function(data) {
+                                        nodes=[];
+
+                                        for (var key in data) {
+                                                tnode={}
+                                                tnode['text']=data[key].name;
+                                                tnode['id']=data[key].id;
+                                                tnode['tags']=[]
+                                                tnode['tags'].push(data[key].uid+" : " + data[key].gid + " : " + parseInt(data[key].mode,10).toString(8));
+                                                if (data[key]['type']=="dir") {
+                                                        tnode['icon']="glyphicon glyphicon-folder-close";
+                                                        tnode['lazyLoad']=true;
+                                                        tnode['showTags']=true;
+                                                        /* You can add more tags for folders here */
+                                                } else {
+                                                        tnode['icon']="glyphicon glyphicon-open-file";
+                                                        tnode['tags'].push(formatCount(data[key].blocks + "Blks"));
+                                                        tnode['tags'].push(formatBytes(data[key].size));
+                                                        /* You can add more tags for files here */
+
+                                                }
+
+                                                nodes.push(tnode);
+                                        }
+                                        loader(nodes);
+                                });
+
+                        }, /* end of lazyload */
+
+                    });
+        });
+
+}
