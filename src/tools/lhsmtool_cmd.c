d993a9e3b6d5dd344c0ff43f5930111778c6b050
Ryan Dubina <ryan.dubina@pnnl.gov>
2024-09-03 17:02:42 -0700

    rename gettid() to sys_gettid() to avoid clash with glibc
    Change-Id: I9a4282f0dcc19e62808c1335046095390f37ef76
    Reviewed-on: https://review.gerrithub.io/c/cea-hpc/robinhood/+/1200340
    Reviewed-by: Thomas Leibovici <thomas.leibovici@cea.fr>
    Tested-by: Thomas Leibovici <thomas.leibovici@cea.fr>

--- src/tools/lhsmtool_cmd.c
+++ src/tools/lhsmtool_cmd.c
@@ -167,7 +167,7 @@ static inline double ct_now(void)
 	return tv.tv_sec + 0.000001 * tv.tv_usec;
 }
 
-static inline pid_t gettid(void)
+static inline pid_t sys_gettid(void)
 {
 	return syscall(SYS_gettid);
 }
@@ -175,12 +175,12 @@ static inline pid_t gettid(void)
 #define LOG_ERROR(_rc, _format, ...)					\
 	llapi_error(LLAPI_MSG_ERROR, _rc,				\
 		    "%f %s[%d]: "_format,				\
-		    ct_now(), cmd_name, gettid(), ## __VA_ARGS__)
+		    ct_now(), cmd_name, sys_gettid(), ## __VA_ARGS__)
 
 #define LOG_DEBUG(_format, ...)						\
 	llapi_error(LLAPI_MSG_DEBUG | LLAPI_MSG_NO_ERRNO, 0,		\
 		    "%f %s[%d]: "_format,				\
-		    ct_now(), cmd_name, gettid(), ## __VA_ARGS__)
+		    ct_now(), cmd_name, sys_gettid(), ## __VA_ARGS__)
 
 static void usage(const char *name, int rc)
 {
