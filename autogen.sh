f52a534b9592f94f284eb785a4efd0b1e9000fd2
Aurelien Degremont <adegremont@nvidia.com>
2025-01-22 08:26:53 -0800

    autogen.sh: do not install git hooks outside of git checkout
    Verify we are in a git-checkout directory before trying
    to setup git-hooks.
    
    Run 'autoreconf' last to ensure autogen.sh exit code is
    autoreconf exit code, to catch autoreconf failures.
    
    Change-Id: I147a7f6406d3f4ce23586afff728d35378bb51a6
    Signed-off-by: Aurelien Degremont <adegremont@nvidia.com>
    Reviewed-on: https://review.gerrithub.io/c/cea-hpc/robinhood/+/1210788
    Reviewed-by: Thomas Leibovici <thomas.leibovici@cea.fr>
    Tested-by: Thomas Leibovici <thomas.leibovici@cea.fr>

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-autoreconf --install
-
-wdir=$(dirname $(readlink -m "$0"))
 
 function install_hook
 {
     local src_file="$1"
     local tgt_file="$2"
+
+    test ! -d "$wdir/.git/" && return 0
 
     if [ ! -e "$wdir/.git/hooks/$tgt_file" ]; then
         echo "installing git hook: $tgt_file"
@@ -17,6 +16,9 @@ function install_hook
     fi
 }
 
+wdir=$(dirname $(readlink -m "$0"))
 install_hook git_prepare_hook prepare-commit-msg
 install_hook pre-commit pre-commit
 install_hook commit-msg commit-msg
+
+autoreconf --install
